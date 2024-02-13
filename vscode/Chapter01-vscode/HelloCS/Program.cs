27241bf826740852a73f565b9fa8ad53de546e93
owusufrk <owusufrk@kean.edu>
2024-02-13 18:27:39 -0500

    First Commit

--- vscode/Chapter01-vscode/HelloCS/Program.cs
+++ vscode/Chapter01-vscode/HelloCS/Program.cs
@@ -1,4 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, C#!");
-throw new Exception();
+string name = "Kwaku O.";
+Console.WriteLine("Hello, " + name + "!");
+Console.WriteLine("Version: {0}", Environment.Version.ToString());
 int z;
+z = 2024;
+Console.WriteLine(z);
+
+// throw new Exception();
+
